b783908d44396dfc85e9bde1eabdcd55e2b0ed41
Nathan Van Heyst <57072440+nvanheyst@users.noreply.github.com>
2024-12-16 17:55:22 -0500

    Update ntpd.sh

--- launch/ntpd.sh
+++ launch/ntpd.sh
@@ -3,6 +3,6 @@
 source /opt/ros/noetic/setup.bash
 source /home/administrator/ntpd_ws/devel/setup.bash
 
-echo "Launching driver!"
+echo "Driver launched"
 
 rosrun ntpd_driver shm_driver _shm_unit:=0 _time_ref_topic:=/swiftnav/position_receiver_0/ros/time_ref
