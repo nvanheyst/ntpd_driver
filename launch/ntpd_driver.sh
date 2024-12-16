79ac417bd56e08b59a44eb3f47f009779adbc7e5
Nathan Van Heyst <57072440+nvanheyst@users.noreply.github.com>
2024-12-16 15:54:18 -0500

    Create ntpd_driver.sh

---
+++ launch/ntpd_driver.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+  
+source /opt/ros/noetic/setup.bash
+source /home/administrator/ntpd_ws/devel/setup.bash
+
+echo "Launching driver!"
+
+rosrun ntpd_driver shm_driver _shm_unit:=0 _time_ref_topic:=/swiftnav/position_receiver_0/ros/time_ref
