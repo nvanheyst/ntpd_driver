123f641a7d9827c36b1086d3e8ac8bc0a7f56cce
Nathan Van Heyst <57072440+nvanheyst@users.noreply.github.com>
2024-12-16 20:47:02 -0500

    Update install.sh

--- launch/install.sh
+++ launch/install.sh
@@ -3,6 +3,28 @@
 # Get the current working directory (this will be the workspace)
 WORKDIR=$(pwd)
 
+# Create the run script in the launch folder
+RUN_SCRIPT="${WORKDIR}/src/ntpd_driver/launch/run_ntpd.sh"
+
+cat <<EOF > $RUN_SCRIPT
+#!/usr/bin/env bash
+
+# Source ROS and workspace setup
+source /opt/ros/noetic/setup.bash
+source ${WORKDIR}/devel/setup.bash
+
+# Run the ROS node
+rosrun ntpd_driver shm_driver _shm_unit:=0 _time_ref_topic:=/swiftnav/position_receiver_0/ros/time_ref
+
+echo "Driver launched"
+EOF
+
+# Make the run script executable
+chmod +x $RUN_SCRIPT
+
+echo "Run script created at ${RUN_SCRIPT} and made executable."
+
+
 # Create the systemd service file for ntpd_service
 cat <<EOF | sudo tee /etc/systemd/system/ntpd_service.service
 [Unit]
@@ -12,8 +34,8 @@ After=network.target ros.service
 [Service]
 Type=simple
 User=root
-WorkingDirectory=${WORKDIR}
-ExecStart=/bin/bash ./ntpd.sh
+WorkingDirectory=${WORKDIR}/src/ntpd_driver/launch/
+ExecStart=/bin/bash ./run_ntpd.sh
 Restart=on-failure
 Environment=ROS_MASTER_URI=http://localhost:11311
 Environment=ROS_IP=127.0.0.1
@@ -35,23 +57,3 @@ sudo systemctl start ntpd_service.service
 
 echo "Service ntpd_service.service has been installed and started successfully."
 
-# Create the run script in the launch folder
-RUN_SCRIPT="${WORKDIR}/src/ntpd_driver/launch/run_ntpd.sh"
-
-cat <<EOF > $RUN_SCRIPT
-#!/usr/bin/env bash
-
-# Source ROS and workspace setup
-source /opt/ros/noetic/setup.bash
-source ${WORKDIR}/devel/setup.bash
-
-# Run the ROS node
-rosrun ntpd_driver shm_driver _shm_unit:=0 _time_ref_topic:=/swiftnav/position_receiver_0/ros/time_ref
-
-echo "Driver launched"
-EOF
-
-# Make the run script executable
-chmod +x $RUN_SCRIPT
-
-echo "Run script created at ${RUN_SCRIPT} and made executable."
